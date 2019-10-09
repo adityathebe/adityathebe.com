5becb4eb2e32772e7303c2155666bd2902edcf7b
Aditya <thebeaditya@gmail.com>
2019-10-09 17:18:52 +0545

    Removed tracedSVG

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,6 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 800,
-              tracedSVG: true,
             },
           },
           {
