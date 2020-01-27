d38dbc26bd8a1f4256eb6a7efdf51fc3951f2ac0
Aditya <thebeaditya@gmail.com>
2020-01-27 21:43:31 +0545

    Removed handleClick

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,11 +24,7 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" keywords={['aditya thebe', 'adityathebe']} />
       <div className="home">
-        <div
-          onClick={changeTheme}
-          className="center-block"
-          onKeyDown={this.handleClick}
-        >
+        <div onClick={changeTheme} className="center-block">
           <Img alt="Author's Image" fixed={data.file.childImageSharp.fixed} />
           <h2>Random Thoughts Of A Programmer</h2>
         </div>
