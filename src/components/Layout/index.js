4ee0b435f1b8570300ba6d6ed9dcab3f21317543
Aditya <thebeaditya@gmail.com>
2020-07-31 15:11:29 +0545

    Removed Simple Analytics

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -14,10 +14,6 @@ const Layout = ({ children }) => (
       <div className="wrapper">{children}</div>
     </main>
     <Footer />
-    <script async defer src="https://cdn.simpleanalytics.io/hello.js" />
-    <noscript>
-      <img src="https://api.simpleanalytics.io/hello.gif" alt="" />
-    </noscript>
   </>
 );
 
