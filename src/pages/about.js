7a920ea1089d7f9f1c1175b0ad0db4046e4ab11f
Aditya <thebeaditya@gmail.com>
2021-04-03 19:03:55 +0545

    Updated About Me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,10 +10,10 @@ const IndexPage = () => (
     <div>
       <h1>About Me ...</h1>
       <p>
-        Describing myself is one of the many things that I'm terrible at. I
-        know, this is pretty taxing for most of us but I, in particular, fail at
-        it horribly. But heck, I'm just gonna try and come up with few words for
-        the one person that's gonna read this - you !
+        I am a software engineer currently working remotely, from Nepal, as a
+        Backend engineer for a startup in Silicon valley. I'm pretty comfotable
+        with NodeJS but recently my interest has shifted to Go programming
+        language.
       </p>
 
       <p>
@@ -22,12 +22,10 @@ const IndexPage = () => (
       </p>
 
       <p>
-        I love playing guitar but certainly nowhere near as much as I used to. I
-        hated playing acoustic guitars because they were much harder on my
-        fingers than an electric guitar. My friend, whose name I don't wanna
-        mention, lost my electric guitar and since then I've transitioned into
-        playing an acoustic guitar. Just kidding - it was Hillihang Limbu who
-        lost my guitar.
+        I love playing guitar but certainly nowhere near as much as I used to.
+        My friend, whose name I don't wanna mention, but his name is Hillihang,
+        lost my electric guitar and since then I've transitioned into playing an
+        acoustic guitar.
       </p>
 
       <p>
@@ -39,16 +37,13 @@ const IndexPage = () => (
       </p>
 
       <p>
-        I'm also a massive ArsenalFC fan. If there's a game, you bet I'm
-        watching it. This also means I've to stay awake late night. Being an
-        Arsenal fan is not good for my heart because of how dire the defenders
-        can be sometimes.
+        I'm also a massive ArsenalFC fan. If there's an Arsenal game, you bet
+        I'm watching it.
       </p>
 
       <p>
-        Speaking of healthy heart, I ocassionaly{' '}
-        <a href="https://www.strava.com/athletes/42641291">run</a>. I run wayyy
-        less than I'd like to.
+        I ocassionaly <a href="https://www.strava.com/athletes/42641291">run</a>
+        , but nowhere near as much as I would like to.
       </p>
     </div>
   </Layout>
