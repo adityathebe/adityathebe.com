045a2fe472e192addf1187995650bef2ea42a3f6
Aditya <thebeaditya@gmail.com>
2021-04-10 12:21:47 +0545

    Updated About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,16 +11,26 @@ const IndexPage = () => (
       <h1>About Me ...</h1>
       <p>
         I am a software engineer currently working remotely, from Nepal, as a
-        Backend engineer for a startup in Silicon valley. I'm pretty comfotable
-        with NodeJS but recently my interest has shifted to Go programming
+        Backend engineer for a startup overseas. I'm pretty comfortable with
+        NodeJS but recently my interest has shifted to the Go programming
         language.
       </p>
 
       <p>
-        Most of the things that I like exists as 1's and 0's. I spend waayyyy
-        too much time in front of my computer.
+        Besides developing software, I am also interested in making them secure
+        and finding security vulnerabilities in web applications. I have helped secure
+        numerous organizations including Harvard University, Cambridge
+        University, Topcoder, Mastercard, Etsy, American Airlines, Comcast,
+        Adobe, Ford, Logitech, ...
       </p>
 
+      <p>
+        In my free time, I play{' '}
+        <a href="https://www.chess.com/member/adityathebe">Chess</a> - in
+        particular Speed Chess. I find speed chess much more entertaining than
+        any other format of chess because of how much of a rush the whole game
+        is and all the hilarious blunders I and my opponent make.
+      </p>
       <p>
         I love playing guitar but certainly nowhere near as much as I used to.
         My friend, whose name I don't wanna mention, but his name is Hillihang,
@@ -29,21 +39,14 @@ const IndexPage = () => (
       </p>
 
       <p>
-        As of recent, I have been playing a lot of{' '}
-        <a href="https://www.chess.com/member/adityathebe">Chess</a> - in
-        particular Speed Chess. I find speed chess much more entertaining than
-        any other format of chess because of how much of a rush the whole game
-        is and all the hilarious blunders I and my opponent make.
-      </p>
-
-      <p>
         I'm also a massive ArsenalFC fan. If there's an Arsenal game, you bet
         I'm watching it.
       </p>
 
       <p>
-        I ocassionaly <a href="https://www.strava.com/athletes/42641291">run</a>
-        , but nowhere near as much as I would like to.
+        I occasionally{' '}
+        <a href="https://www.strava.com/athletes/42641291">run</a>, but nowhere
+        near as much as I would like to.
       </p>
     </div>
   </Layout>
