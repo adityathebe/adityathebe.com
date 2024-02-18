d665610b07ace155806703b1409d1c130fe3ae13
Aditya Thebe <contact@adityathebe.com>
2024-02-18 14:47:57 +0545

    chore: remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-yaml`,
-    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
