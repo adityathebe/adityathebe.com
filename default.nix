f628f30725ac69b94b96742a2f1e1318110bd57c
Aditya Thebe <contact@adityathebe.com>
2024-02-18 13:07:29 +0545

    chore: bump all packages

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
   name = "nodejs";
   buildInputs = with pkgs; [
     pkgs.libuuid
+    yarn
     nodejs_18
   ];
   env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];  }; 
