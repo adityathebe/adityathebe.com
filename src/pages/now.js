d8f7f20af924984d0c6840208729565e0e07099f
Aditya Thebe <contact@adityathebe.com>
2024-04-06 14:19:24 +0545

    chore: update /now page

--- src/pages/now.js
+++ src/pages/now.js
@@ -10,16 +10,24 @@ const IndexPage = () => (
     <div className="post-content">
       <h1 id="page-title">Now</h1>
       <ul>
-        <li>Working at <a href="https://flanksource.com">@Flanksource</a></li>
-        <li><a href="https://hevy.com/user/adityathebe">Working out</a></li>
-        <li>Working on redesigning this website and adding new stuff</li>
-        <li>Using neovim part-time. Hoping to move to it soon</li>
-        <li>Learning <a href="https://github.com/adityathebe/nixos-config">NixOS</a></li>
-        <li>Building <a href="https://github.com/adityathebe/homelab">homelab</a></li>
+        <li>
+          Working at <a href="https://flanksource.com">@Flanksource</a>
+        </li>
+        <li>
+          <a href="https://hevy.com/user/adityathebe">Working out</a>
+        </li>
+        <li>In the process of using neovim full-time</li>
+        <li>
+          Reading <a href="https://theartofpostgresql.com/">The Art Of PostgreSQL</a>
+        </li>
+        <li>Playing Pubg</li>
+        <li>
+          Watching <a href="https://www.imdb.com/title/tt2788316/">Shogun</a>
+        </li>
       </ul>
     </div>
 
-    <p>Last Updated: 28 Feb 2024</p>
+    <p>Last Updated: 06 Apr 2024</p>
   </Layout>
 );
 
