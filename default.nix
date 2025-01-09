16a405eafea3a3c93b76b14491190c93aa652fb5
Aditya Thebe <contact@adityathebe.com>
2025-01-09 23:43:41 +0545

    chore: migrate from yarn to npm (#32)
    one fewer tool

--- default.nix
+++ default.nix
@@ -3,7 +3,6 @@ pkgs.mkShell {
   name = "nodejs";
   buildInputs = with pkgs; [
     pkgs.libuuid
-    yarn
     nodejs_20
   ];
   env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];  }; 
