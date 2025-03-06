ff16d5db00bed93d4a8fced08e2bca52abdce0d5
Aditya Thebe <contact@adityathebe.com>
2025-03-06 20:16:56 +0545

    chore: update /uses and /now

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM --platform=linux/arm64 node:20-bullseye
+FROM node:20-bullseye
 
 WORKDIR /app
 
--- src/pages/now.js
+++ src/pages/now.js
@@ -14,21 +14,21 @@ const IndexPage = () => (
           Working at <a href="https://flanksource.com">@Flanksource</a>
         </li>
         <li>
+          Learning to drive
+        </li>
+        {/* <li>
           <a href="https://hevy.com/user/adityathebe">Working out</a>
-        </li>
-        <li>
-          Binging <a href="https://www.rottentomatoes.com/tv/homeland">Homeland (@season 4)</a>
-        </li>
-        <li>
+        </li> */}
+        {/* <li>
           Taking{' '}
           <a href="https://www.youtube.com/playlist?list=PLSE8ODhjZXjYDBpQnSymaectKjxCy6BYq">
             CMU Intro to Database Systems
           </a>
-        </li>
+        </li> */}
       </ul>
     </div>
 
-    <p>Last Updated: 28 Jan 2025</p>
+    <p>Last Updated: 06 Mar, 2025</p>
   </Layout>
 );
 
