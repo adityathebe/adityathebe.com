3d2b9e8b516e35a413e8891d440ef480577d957f
Aditya Thebe <contact@adityathebe.com>
2025-08-09 22:56:31 +0545

    fix: title

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -28,13 +28,13 @@ export function Head({ description = '', keywords = [], title = '', featuredImag
       <title>{pageTitle}</title>
       <meta name="yandex-verification" content="0e3d760f663fd964" />
       <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={featuredImage || fallbackSocialImage} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={site.siteMetadata.author} />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="twitter:image" content={featuredImage || fallbackSocialImage} />
       {keywords.length > 0 && <meta name="keywords" content={keywords.join(`, `)} />}
