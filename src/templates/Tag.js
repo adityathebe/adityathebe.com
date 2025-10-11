8579133d2e327ec049e10463fa2e29cb353f7e9f
Aditya Thebe <contact@adityathebe.com>
2025-10-11 14:39:19 +0545

    feat: Show journal and post together in the tags page

--- src/templates/Tag.js
+++ src/templates/Tag.js
@@ -7,67 +7,79 @@ import { Head as SEOHead } from '../components/SEO';
 import { formatPostDate } from '../utils/helper.js';
 import { tagPath, formatTagLabel } from '../utils/tags.js';
 
+/**
+ * @typedef {Object} ContentNode
+ * @property {string} id
+ * @property {string} fileAbsolutePath
+ * @property {Object} frontmatter
+ * @property {string} frontmatter.title
+ * @property {string} frontmatter.date
+ * @property {string} frontmatter.slug
+ * @property {string[]} frontmatter.categories
+ */
+
+/**
+ * @typedef {Object} ContentEdge
+ * @property {ContentNode} node
+ */
+
+/**
+ * @param {Object} props
+ * @param {any} props.data
+ * @param {Object} props.pageContext
+ * @param {string} [props.pageContext.tagSlug]
+ * @param {string[]} [props.pageContext.tagAliases]
+ */
 const TagTemplate = ({ data, pageContext }) => {
+  /** @type {ContentEdge[]} */
   const allContent = data.allMarkdownRemark.edges;
   const tagDisplayName = formatTagLabel(pageContext.tagSlug || pageContext.tagAliases?.[0] || '');
-
-  // Separate posts and journals
-  const posts = allContent.filter(({ node }) => node.fileAbsolutePath.includes('/Posts/'));
-  const journals = allContent.filter(({ node }) => node.fileAbsolutePath.includes('/WeeklyJournal/'));
-
-  const renderList = (items, emptyMessage) => {
-    if (items.length === 0) {
-      return <p>{emptyMessage}</p>;
-    }
-    return (
-      <ul className="post-list">
-        {items.map(({ node }) => (
-          <li key={node.id}>
-            <div className="post-content-wrapper">
-              <span className="post-date">
-                <span className="post-date-desktop">{formatPostDate(node.frontmatter.date).short}</span>
-                <span className="post-date-mobile">{formatPostDate(node.frontmatter.date).full}</span>
-              </span>
-
-              <div className="post-item">
-                <Link className="post-link" to={node.frontmatter.slug}>
-                  {node.frontmatter.title}
-                </Link>
-
-                <div className="post-meta">
-                  {(node.frontmatter.categories || []).map((category, idx) => (
-                    <Link key={idx} className="post-tag" to={tagPath(category)}>
-                      {formatTagLabel(category)}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
-  };
 
   return (
     <Layout>
       <div className="tag-page">
-        <h2 className="post-header">Posts tagged "{tagDisplayName}"</h2>
-        {renderList(posts, 'No posts found.')}
+        <h2 className="post-header">Tagged "{tagDisplayName}"</h2>
+        {allContent.length === 0 ? (
+          <p>No content found.</p>
+        ) : (
+          <ul className="post-list">
+            {allContent.map(({ node }) => (
+              <li key={node.id}>
+                <div className="post-content-wrapper">
+                  <span className="post-date">
+                    <span className="post-date-desktop">{formatPostDate(node.frontmatter.date).short}</span>
+                    <span className="post-date-mobile">{formatPostDate(node.frontmatter.date).full}</span>
+                  </span>
 
-        {journals.length > 0 && (
-          <>
-            <h2 className="post-header" style={{ marginTop: '3rem' }}>
-              Weekly Journals tagged "{tagDisplayName}"
-            </h2>
-            {renderList(journals, 'No journals found.')}
-          </>
+                  <div className="post-item">
+                    <Link className="post-link" to={node.frontmatter.slug}>
+                      {node.frontmatter.title}
+                    </Link>
+
+                    <div className="post-meta">
+                      {(node.frontmatter.categories || []).map((category, idx) => (
+                        <Link key={idx} className="post-tag" to={tagPath(category)}>
+                          {formatTagLabel(category)}
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
         )}
       </div>
     </Layout>
   );
 };
 
+/**
+ * @param {Object} props
+ * @param {Object} props.pageContext
+ * @param {string} [props.pageContext.tagSlug]
+ * @param {string[]} [props.pageContext.tagAliases]
+ */
 export const Head = ({ pageContext }) => {
   const tagDisplayName = formatTagLabel(pageContext.tagSlug || pageContext.tagAliases?.[0] || '');
   return (
