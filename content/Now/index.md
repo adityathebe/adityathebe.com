e4cf1930edf1189a05bfdb7f6674b5c84b28a812
Aditya Thebe <contact@adityathebe.com>
2025-12-05 00:14:22 +0545

    update now.js

---
+++ content/Now/index.md
@@ -0,0 +1,12 @@
+---
+title: '🕰️ Now'
+slug: '/now'
+description: What I'm focused on right now.
+updated: 2025-12-05
+---
+
+- 🏠 In Kathmandu
+- Working at [@Flanksource](https://flanksource.com)
+- Watching [Fargo Season II](https://www.imdb.com/title/tt2802850/)
+- Building [godejs](https://github.com/godejs/godejs) - The least spec-compliant JavaScript runtime in Go
+- Learning about logs by building [bitcask](https://riak.com/assets/bitcask-intro.pdf) in Go
--- src/pages/now.js
+++ src/pages/now.js
@@ -1,60 +1,50 @@
 // @ts-check
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import { Head as SEOHead } from '../components/SEO';
 import Layout from '../components/Layout';
+import { formatPostDate } from '../utils/helper.js';
 
-const items = [
-  { id: 'location', label: '🏠 In Kathmandu' },
-  {
-    id: 'work',
-    label: 'Working at',
-    link: { href: 'https://flanksource.com', text: '@Flanksource' },
-  },
-  {
-    id: 'watching',
-    label: 'Watching',
-    link: { href: 'https://www.imdb.com/title/tt2802850/', text: 'Fargo Season II' },
-  },
-  // {
-  //   id: 'reading',
-  //   label: 'Reading',
-  //   link: {
-  //     href: 'https://www.oreilly.com/library/view/designing-data-intensive-applications/9781491903063/',
-  //     text: 'Designing Data-Intensive Applications',
-  //   },
-  // },
-  // {
-  //   id: 'workout',
-  //   label: 'Working out',
-  //   link: { href: 'https://hevy.com/user/adityathebe', text: 'Hevy' },
-  // },
-];
+const IndexPage = ({ data }) => {
+  const now = data?.markdownRemark;
+  const title = now?.frontmatter?.title || 'Now';
+  const updated = now?.frontmatter?.updated ? formatPostDate(now.frontmatter.updated).full : null;
 
-const IndexPage = () => (
-  <Layout>
-    <div className="post-content">
-      <h1 id="page-title">🕰️ Now</h1>
-      <br />
-      <ul>
-        {items.map(({ id, label, link }) => (
-          <li key={id}>
-            {label}
-            {link ? (
-              <>
-                {' '}
-                <a href={link.href}>{link.text}</a>
-              </>
-            ) : null}
-          </li>
-        ))}
-      </ul>
-    </div>
+  return (
+    <Layout>
+      <div className="post-content">
+        <h1 id="page-title">{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: now?.html || '' }} />
+      </div>
 
-    <p>Last Updated: 4 Dec, 2025</p>
-  </Layout>
-);
+      {updated ? <p>Last Updated: {updated}</p> : null}
+    </Layout>
+  );
+};
 
-export const Head = () => <SEOHead title="Now" keywords={['aditya thebe currently', 'aditya thebe now']} />;
+export const Head = ({ data }) => {
+  const frontmatter = data?.markdownRemark?.frontmatter || {};
+  return (
+    <SEOHead
+      title={frontmatter.title || 'Now'}
+      description={frontmatter.description}
+      keywords={['aditya thebe currently', 'aditya thebe now']}
+    />
+  );
+};
+
+export const query = graphql`
+  query NowPage {
+    markdownRemark(frontmatter: { slug: { eq: "/now" } }) {
+      html
+      frontmatter {
+        title
+        description
+        updated
+      }
+    }
+  }
+`;
 
 export default IndexPage;
